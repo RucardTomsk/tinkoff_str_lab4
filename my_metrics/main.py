8566633473a5f77e0ea8f46408176fcb1e16498c
Rucard <www.carat@list.ru>
2023-10-08 09:10:53 +0700

    first work ^)

--- my_metrics/main.py
+++ my_metrics/main.py
@@ -1,12 +1,22 @@
 from http.server import HTTPServer
-from prometheus_client import Gauge, MetricsHandler
+import prometheus_client
+from prometheus_client import Counter, start_http_server
 import logging
 import requests
 import yaml
 import json
+import time
+from time import sleep
+from threading import Thread
+import datetime
 
+logging.basicConfig(level=logging.INFO, filename="service_log.log", filemode="w",
+                    format="%(asctime)s %(levelname)s %(message)s")
 logger = logging.getLogger()
 
+prometheus_client.REGISTRY.unregister(prometheus_client.GC_COLLECTOR)
+prometheus_client.REGISTRY.unregister(prometheus_client.PLATFORM_COLLECTOR)
+prometheus_client.REGISTRY.unregister(prometheus_client.PROCESS_COLLECTOR)
 
 class prometheus(object):
     def __init__(self, config):
@@ -16,18 +26,16 @@ class prometheus(object):
             logger.error('prometheus server_port not present in config.')
             return
 
-        self.gauges = {}
+        self.counters = {}
 
         logger.info('Starting prometheus metrics web server at %s', port)
-        HTTPServer(("0.0.0.0", port), MetricsHandler).serve_forever()
+        start_http_server(port)
 
-    def send_metrics(self, metrics):
-        for metric, value in metrics.items():
-            if metric not in self.gauges:
-                self.gauges[metric] = Gauge(metric, '')
-            logger.info('Setting metrics gauge %s to %s', metric, value)
-            self.gauges[metric].set_to_current_time()
-            self.gauges[metric].set(value)
+    def send_metrics(self, metric):
+        if metric not in self.counters:
+            self.counters[metric] = Counter(metric, '')
+        logger.info('Setting metrics counter %s', metric)
+        self.counters[metric].inc()
 
 
 class oncall_api(object):
@@ -36,20 +44,67 @@ class oncall_api(object):
         self.session = requests.Session()
         self.__login(username=config['oncall_api']['username'], password=config['oncall_api']['password'])
 
-    def __login(self,username, password = 123):
-        response = self.session.post(f"{self.url}/login", data={
-            'username': username,
-            'password': password
-        })
+    def __login(self, username, password=123):
+        try:
+            response = self.session.post(f"{self.url}/login", data={
+                'username': username,
+                'password': password
+            })
+        except requests.exceptions.ConnectionError:
+            logger.error(f"API: Failed connection {config['oncall_api']['base_url']}")
+
 
         if response.status_code == 200:
             self.csrf_token = response.json()['csrf_token']
         else:
-            logger.warning("API: Failed authorisation attempt")
+            logger.warning(f"API: Failed authorisation attempt.[{response.status_code}] {response.json()}")
+
+    def get(self, request_path, query_params={}):
+        response = self.session.get(f"{self.url}{request_path}", params=query_params)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            logger.warning(f"API: Failed get request.[{response.status_code}] {response.json()}")
+            return None
+
+def date_to_unixtime(date):
+    return time.mktime(date.timetuple())
+
+def metric1(_prometheus: prometheus, api: oncall_api):
+    while True:
+        name_metric = "number_of_off_duty_hours"
+        teams = api.get("/api/v0/teams")
+        if teams is None:
+            continue
+
+        today = datetime.date.today()
+        tomorrow = today + datetime.timedelta(days=1)
+
+        for team in teams:
+            events = api.get("/api/v0/events", query_params={
+                'team': team,
+                'start__ge': date_to_unixtime(today),
+                'start__lt': date_to_unixtime(tomorrow),
+                'end__ge': date_to_unixtime(tomorrow),
+            })
+            if events is None:
+                continue
+
+            if len(events) == 0:
+                _prometheus.send_metrics(name_metric)
+
+        sleep(2)
+
 
 
 with open('config.yml') as fh:
     config = yaml.load(fh, Loader=yaml.FullLoader)
     fh.close()
 
-api = oncall_api(config)
+if __name__ == "__main__":
+    api = oncall_api(config)
+    prom = prometheus(config)
+    m1 = Thread(target=metric1, args=(prom, api,))
+    m1.start()
+
+
